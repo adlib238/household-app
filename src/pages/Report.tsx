53f62d5038123fcfe2bbda249fcbd4bf76ec12c9
adlib238 <f.kadlib238@gmail.com>
2024-05-07 11:35:51 +0900

    add Report selectDate

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { Grid, Paper } from "@mui/material";
+import MonthSelector from "../components/MonthSelector";
+import CategoryChart from "../components/CategoryChart";
+import TransactionTable from "../components/TransactionTable";
+import BarChart from "../components/BarChart";
 
-const Report = () => {
-  return <div>Report</div>;
+interface ReportProps {
+  // monthlyTransactions: Transaction[];
+  currentMonth: Date;
+  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>; //追加
+  // onDeleteTransaction: (ids: string | readonly string[]) => Promise<void>;
+  // isLoading: boolean;
+}
+
+const Report = ({ currentMonth, setCurrentMonth }: ReportProps) => {
+  const commonPaperStyle = {
+    height: "400px",
+    display: "flex",
+    flexDirection: "column",
+    p: 2,
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        {/* 日付選択エリア */}
+        <MonthSelector
+          currentMonth={currentMonth}
+          setCurrentMonth={setCurrentMonth}
+        />
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Paper sx={commonPaperStyle}>
+          <CategoryChart />
+        </Paper>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Paper sx={commonPaperStyle}>
+          <BarChart />
+        </Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <TransactionTable />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Report;
